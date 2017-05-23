ec9ca16cc84243ed44365fd238596b224d90060b
Varun Gupta <vgupta16@gmail.com>
2017-05-23 16:49:21 -0400

    allow 0 values for nf fields when building full nutrients; do not allow negative values

--- src/index.js
+++ src/index.js
@@ -102,9 +102,9 @@ function convertV1ItemToTrackFood(v1Item, defaultObj) {
  */
 function buildFullNutrientsArray(data) {
   return _.reduce(nutrientsMap, function(accum, nutrDetails, v1AttrName) {
-    if (data[v1AttrName]) {
+    if (data[v1AttrName] || data[v1AttrName] === 0) {
       let value   = parseFloat(data[v1AttrName]);
-      if (!isNaN(value)) {
+      if (!isNaN(value) && !(value < 0)) {
         let attr_id = nutrDetails.attr_id;
         //ensure that daily value measures are calculated into the appropriate units.
         if (dailyValueTransforms[attr_id]) {
